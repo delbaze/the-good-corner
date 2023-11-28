5908105c03d089eea584c28651766a7cb3df6e38
delbaze <56109313+delbaze@users.noreply.github.com>
2023-11-28 16:37:47 +0100

    dockerisation

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,10 +1,22 @@
 import { DataSource } from "typeorm";
 
 export default new DataSource({
-  type: "sqlite",
-  database: "the_good_corner.sqlite",
-  entities: ["src/entities/*.ts"],
+  type: "postgres",
+  host: "db",
+  port: 5432,
+  username: "postgres",
+  password: "postgres",
+  database: "thegoodcorner",
   synchronize: true,
-  // logging: ["query","error"],
   logging: true,
+  entities: ["src/entities/*.ts"],
 });
+
+// export default new DataSource({
+//   type: "sqlite",
+//   database: "the_good_corner.sqlite",
+//   entities: ["src/entities/*.ts"],
+//   synchronize: true,
+//   // logging: ["query","error"],
+//   logging: true,
+// });
